205810b4013fac7f07952a60c69e682260369508
gohkh <gohkh2011@gmail.com>
2020-03-16 17:34:42 +0800

    Implement RecipeAddCommandParser

---
+++ src/main/java/seedu/address/logic/commands/recipe/RecipeAddStepCommand.java
@@ -0,0 +1,15 @@
+package seedu.address.logic.commands.recipe;
+
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Adds a step to the address book.
+ */
+public class RecipeAddStepCommand extends RecipeAddCommand {
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException("Not yet implemented");
+    }
+}
