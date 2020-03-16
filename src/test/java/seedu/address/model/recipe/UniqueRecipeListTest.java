8c06d50e689cae4d819435535de8daa04adc2dbd
waaaflesss <50788391+waaaflesss@users.noreply.github.com>
2020-03-17 00:21:55 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/model/recipe/UniqueRecipeListTest.java
+++ src/test/java/seedu/address/model/recipe/UniqueRecipeListTest.java
@@ -1,10 +1,10 @@
 package seedu.address.model.recipe;
 
-import static seedu.address.testutil.TypicalRecipes.CARBONARA;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalRecipes.CARBONARA;
 
 import org.junit.jupiter.api.Test;
 
