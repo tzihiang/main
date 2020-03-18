348f468c5aaeb6afb7854a4d1571f0188f87874a
Willis Tay <27534078+tzihiang@users.noreply.github.com>
2020-03-18 14:23:40 +0800

    Fixed some checktyle issues

--- src/main/java/seedu/address/logic/commands/cart/CartRemoveIngredientCommand.java
+++ src/main/java/seedu/address/logic/commands/cart/CartRemoveIngredientCommand.java
@@ -1,11 +1,11 @@
 package seedu.address.logic.commands.cart;
 
+import static java.util.Objects.requireNonNull;
+
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ingredient.Ingredient;
-
-import static java.util.Objects.requireNonNull;
 
 /**
  * Adds an ingredient to the cart
