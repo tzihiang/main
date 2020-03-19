93a2d5f2be3c098d08574b7e18a4467922abe7bc
Willis Tay <27534078+tzihiang@users.noreply.github.com>
2020-03-19 15:44:51 +0800

    Fixed appveyor issues

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -16,10 +16,7 @@ import org.junit.jupiter.api.Test;
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.AddressBook;
-import seedu.address.model.Model;
-import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.*;
 import seedu.address.model.ingredient.Ingredient;
 import seedu.address.model.person.Person;
 import seedu.address.model.recipe.Recipe;
@@ -157,6 +154,11 @@ public class AddCommandTest {
 
         @Override
         public void setRecipe(Recipe target, Recipe editedPerson) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setInventory(Inventory target, Inventory editedInventory) {
             throw new AssertionError("This method should not be called.");
         }
 
