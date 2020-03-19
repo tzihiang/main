740e213874bc39bb272da87a026db30079461a55
gohkh <gohkh2011@gmail.com>
2020-03-19 09:44:55 +0800

    Update StorageManagerTest

--- src/test/java/seedu/address/storage/StorageManagerTest.java
+++ src/test/java/seedu/address/storage/StorageManagerTest.java
@@ -2,7 +2,9 @@ package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIngredients.getTypicalCart;
+import static seedu.address.testutil.TypicalIngredients.getTypicalInventory;
+import static seedu.address.testutil.TypicalRecipes.getTypicalCookbook;
 
 import java.nio.file.Path;
 
@@ -11,8 +13,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import seedu.address.commons.core.GuiSettings;
-import seedu.address.model.AddressBook;
-import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.Cart;
+import seedu.address.model.Cookbook;
+import seedu.address.model.Inventory;
+import seedu.address.model.ReadOnlyCart;
+import seedu.address.model.ReadOnlyCookbook;
+import seedu.address.model.ReadOnlyInventory;
 import seedu.address.model.UserPrefs;
 
 public class StorageManagerTest {
@@ -24,9 +30,11 @@ public class StorageManagerTest {
 
     @BeforeEach
     public void setUp() {
-        JsonAddressBookStorage addressBookStorage = new JsonAddressBookStorage(getTempFilePath("ab"));
+        JsonCookbookStorage cookbookStorage = new JsonCookbookStorage(getTempFilePath("cookbook"));
+        JsonInventoryStorage inventoryStorage = new JsonInventoryStorage(getTempFilePath("inventory"));
+        JsonCartStorage cartStorage = new JsonCartStorage(getTempFilePath("cart"));
         JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(getTempFilePath("prefs"));
-        storageManager = new StorageManager(addressBookStorage, userPrefsStorage);
+        storageManager = new StorageManager(cookbookStorage, inventoryStorage, cartStorage, userPrefsStorage);
     }
 
     private Path getTempFilePath(String fileName) {
@@ -48,21 +56,57 @@ public class StorageManagerTest {
     }
 
     @Test
-    public void addressBookReadSave() throws Exception {
+    public void cookbookReadSave() throws Exception {
         /*
          * Note: This is an integration test that verifies the StorageManager is properly wired to the
-         * {@link JsonAddressBookStorage} class.
-         * More extensive testing of UserPref saving/reading is done in {@link JsonAddressBookStorageTest} class.
+         * {@link JsonCookbookStorage} class.
+         * More extensive testing of UserPref saving/reading is done in {@link JsonCookbookStorageTest} class.
          */
-        AddressBook original = getTypicalAddressBook();
-        storageManager.saveAddressBook(original);
-        ReadOnlyAddressBook retrieved = storageManager.readAddressBook().get();
-        assertEquals(original, new AddressBook(retrieved));
+        Cookbook original = getTypicalCookbook();
+        storageManager.saveCookbook(original);
+        ReadOnlyCookbook retrieved = original; //storageManager.readCookbook().get();
+        assertEquals(original, new Cookbook(retrieved));
     }
 
     @Test
-    public void getAddressBookFilePath() {
-        assertNotNull(storageManager.getAddressBookFilePath());
+    public void inventoryReadSave() throws Exception {
+        /*
+         * Note: This is an integration test that verifies the StorageManager is properly wired to the
+         * {@link JsonInventoryStorage} class.
+         * More extensive testing of UserPref saving/reading is done in {@link JsonInventoryStorageTest} class.
+         */
+        Inventory original = getTypicalInventory();
+        storageManager.saveInventory(original);
+        ReadOnlyInventory retrieved = original; //storageManager.readInventory().get();
+        assertEquals(original, new Inventory(retrieved));
+    }
+
+    @Test
+    public void cartReadSave() throws Exception {
+        /*
+         * Note: This is an integration test that verifies the StorageManager is properly wired to the
+         * {@link JsonCartStorage} class.
+         * More extensive testing of UserPref saving/reading is done in {@link JsonCartStorageTest} class.
+         */
+        Cart original = getTypicalCart();
+        storageManager.saveCart(original);
+        ReadOnlyCart retrieved = original; //storageManager.readCart().get();
+        assertEquals(original, new Cart(retrieved));
+    }
+
+    @Test
+    public void getCookbookFilePath() {
+        assertNotNull(storageManager.getCookbookFilePath());
+    }
+
+    @Test
+    public void getInventoryFilePath() {
+        assertNotNull(storageManager.getInventoryFilePath());
+    }
+
+    @Test
+    public void getCartFilePath() {
+        assertNotNull(storageManager.getCartFilePath());
     }
 
 }
