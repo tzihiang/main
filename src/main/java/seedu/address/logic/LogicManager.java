604db06857a8ae968702b66dfd1d453b318a4772
hans555 <onghansheng@gmail.com>
2020-03-20 19:02:09 +0800

    connect mainwindow to be able to parse cooking papa command

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -48,7 +48,7 @@ public class LogicManager implements Logic {
         logger.info("----------------[USER COMMAND][" + commandText + "]");
 
         CommandResult commandResult;
-        Command command = addressBookParser.parseCommand(commandText);
+        Command command = cookingPapaParser.parseCommand(commandText);
         commandResult = command.execute(model);
 
         try {
--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -198,8 +198,8 @@ public class MainWindow extends UiPart<Stage> {
         try {
             CommandResult commandResult = logic.execute(commandText);
             logger.info("Result: " + commandResult.getFeedbackToUser());
-            resultDisplay.setFeedbackToUser(commandText);
-            // resultDisplay.setFeedbackToUser(commandResult.getFeedbackToUser());
+            //resultDisplay.setFeedbackToUser(commandText);
+            resultDisplay.setFeedbackToUser(commandResult.getFeedbackToUser());
 
             if (commandResult.isShowHelp()) {
                 handleHelp();
@@ -212,8 +212,8 @@ public class MainWindow extends UiPart<Stage> {
             return commandResult;
         } catch (CommandException | ParseException e) {
             logger.info("Invalid command: " + commandText);
-            resultDisplay.setFeedbackToUser(commandText);
-            // resultDisplay.setFeedbackToUser(e.getMessage());
+            //resultDisplay.setFeedbackToUser(commandText);
+            resultDisplay.setFeedbackToUser(e.getMessage());
             throw e;
         }
     }
