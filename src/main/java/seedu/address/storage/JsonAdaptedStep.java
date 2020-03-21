1ad6ec8fa461b44004eb91c3727ecad9b0210c48
waaaflesss <50788391+waaaflesss@users.noreply.github.com>
2020-03-21 14:23:43 +0800

    Implement Storage for Step

---
+++ src/main/java/seedu/address/storage/JsonAdaptedStep.java
@@ -0,0 +1,50 @@
+package seedu.address.storage;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.ingredient.IngredientName;
+import seedu.address.model.step.Step;
+
+/**
+ * Jackson-friendly version of {@link Step}.
+ */
+class JsonAdaptedStep {
+
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Step's %s field is missing!";
+
+    private final String stepDescription;
+
+    /**
+     * Constructs a {@code JsonAdaptedStep} with the given {@code stepDescription}.
+     */
+    @JsonCreator
+    public JsonAdaptedStep(@JsonProperty("description") String stepDescription) {
+        this.stepDescription = stepDescription;
+    }
+
+    /**
+     * Converts a given {@code Step} into this class for Jackson use.
+     */
+    public JsonAdaptedStep(Step source) {
+        stepDescription = source.getStepDescription();
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted ingredient object into the model's {@code Ingredient} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted ingredient.
+     */
+    public Step toModelType() throws IllegalValueException {
+        if (stepDescription == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    "step description"));
+        } else if (!Step.isValidStep(stepDescription)) {
+            throw new IllegalValueException(Step.MESSAGE_CONSTRAINTS);
+        }
+
+        return new Step(stepDescription);
+    }
+
+}
---
+++ src/test/java/seedu/address/testutil/TypicalSteps.java
@@ -0,0 +1,23 @@
+package seedu.address.testutil;
+
+import seedu.address.model.step.Step;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TypicalSteps {
+
+    public static final Step CARBONARA_ONE = new Step("Boil the pasta");
+    public static final Step CARBONARA_TWO = new Step("Fry pancetta in oil in a frying pan for a few mins "
+            + "until golden and crisp. Add garlic, fry for 1 min, ");
+    public static final Step CARBONARA_THREE = new Step("Turn off the heat.");
+    public static final Step CARBONARA_FOUR = new Step("Briefly whisk egg and yolks with most of the Parmesan "
+            + "and some seasoning.");
+
+    private TypicalSteps() {} // prevents instantiation
+
+    public static List<Step> getTypicalSteps() {
+        return new ArrayList<>(Arrays.asList(CARBONARA_ONE, CARBONARA_TWO, CARBONARA_THREE, CARBONARA_FOUR));
+    }
+}
