6645d40a173f63dd7af736b73eba75a6855487f9
waaaflesss <50788391+waaaflesss@users.noreply.github.com>
2020-03-22 13:40:40 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/storage/JsonAdaptedStepDescriptionTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedStepDescriptionTest.java
@@ -1,13 +1,14 @@
 package seedu.address.storage;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.step.Step;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.storage.JsonAdaptedStepDescription.MISSING_FIELD_MESSAGE_FORMAT;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalSteps.CARBONARA_ONE;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.step.Step;
 
 public class JsonAdaptedStepDescriptionTest {
     private static final String INVALID_STEP_DESCRIPTION = " ";
