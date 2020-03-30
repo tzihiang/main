ad3fedf244dfabd354574e3c0021a5aa44ba6f00
waaaflesss <50788391+waaaflesss@users.noreply.github.com>
2020-03-30 23:48:39 +0800

    Add CartClearCommandTest

---
+++ src/test/java/seedu/address/logic/commands/cart/CartClearCommandTest.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.commands.cart;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Cart;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.testutil.TypicalIngredients;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.logic.commands.cart.CartClearCommand.MESSAGE_SUCCESS;
+
+public class CartClearCommandTest {
+
+    @Test
+    public void execute_success() throws CommandException {
+        Model model = new ModelManager();
+        CartClearCommand c = new CartClearCommand();
+
+        // empty cart
+        assertEquals(c.execute(model), new CommandResult(MESSAGE_SUCCESS));
+
+        // with ingredients inside the cart
+        model.addCartIngredient(TypicalIngredients.ALMOND);
+        model.addCartIngredient(TypicalIngredients.BANANA);
+        model.addCartIngredient(TypicalIngredients.CHICKEN);
+        assertEquals(c.execute(model), new CommandResult(MESSAGE_SUCCESS));
+    }
+}
