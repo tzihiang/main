053719fa55d63625391d29397aeeebfdd1954352
waaaflesss <50788391+waaaflesss@users.noreply.github.com>
2020-04-01 02:41:21 +0800

    Add CookbookViewCommandParserTest

--- src/test/java/seedu/address/logic/parser/cookbook/CookbookViewCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/cookbook/CookbookViewCommandParserTest.java
@@ -6,29 +6,29 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.cookbook.CookbookRemoveCommand;
+import seedu.address.logic.commands.cookbook.CookbookViewCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
-public class CookbookRemoveCommandParserTest {
+public class CookbookViewCommandParserTest {
     private static final Index VALID_INDEX = new Index(0);
     private static final String VALID_INDEX_STRING = "1";
     private static final String INVALID_INDEX_STRING = "Invalid index string";
 
     @Test
     public void parse_validInput() throws ParseException {
-        CookbookRemoveCommandParser c = new CookbookRemoveCommandParser();
-        assertEquals(c.parse(VALID_INDEX_STRING), new CookbookRemoveCommand(VALID_INDEX));
+        CookbookViewCommandParser c = new CookbookViewCommandParser();
+        assertEquals(c.parse(VALID_INDEX_STRING), new CookbookViewCommand(VALID_INDEX));
     }
 
     @Test
     public void parse_null_throwsNullPointerException() {
-        CookbookRemoveCommandParser c = new CookbookRemoveCommandParser();
+        CookbookViewCommandParser c = new CookbookViewCommandParser();
         assertThrows(NullPointerException.class, () -> c.parse(null));
     }
 
     @Test
     public void parse_invalidInput() {
-        CookbookRemoveCommandParser c = new CookbookRemoveCommandParser();
+        CookbookViewCommandParser c = new CookbookViewCommandParser();
         assertThrows(ParseException.class, () -> c.parse(INVALID_INDEX_STRING));
     }
 }
