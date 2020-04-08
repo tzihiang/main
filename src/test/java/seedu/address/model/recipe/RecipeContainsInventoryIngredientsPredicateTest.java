e105c4a767b53aa2a64d70bd833799c222794533
gohkh <gohkh2011@gmail.com>
2020-04-08 15:04:41 +0800

    Update tests

--- src/test/java/seedu/address/model/recipe/RecipeContainsInventoryIngredientsPredicateTest.java
+++ src/test/java/seedu/address/model/recipe/RecipeContainsInventoryIngredientsPredicateTest.java
@@ -2,10 +2,12 @@ package seedu.address.model.recipe;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalRecipes.getValidRecipe;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.Inventory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.ingredient.Ingredient;
@@ -18,10 +20,25 @@ public class RecipeContainsInventoryIngredientsPredicateTest {
     private static final Recipe VALID_RECIPE = getValidRecipe();
 
     @Test
-    public void test_validInput() {
+    public void test_inventoryContainsAllRecipeIngredients_returnsTrue() {
+        Inventory inventory = new Inventory();
+        inventory.setIngredients(VALID_RECIPE.getIngredients().asUnmodifiableObservableList());
+
         Model model = new ModelManager();
         model.addCookbookRecipe(VALID_RECIPE);
-        model.addInventoryIngredient(VALID_INGREDIENT);
+        model.setInventory(inventory);
+
+        RecipeContainsInventoryIngredientsPredicate c =
+            new RecipeContainsInventoryIngredientsPredicate(model.getInventory());
+        assertTrue(c.test(VALID_RECIPE));
+    }
+
+    @Test
+    public void test_inventoryDoesNotContainRecipeIngredients_returnsTrue() {
+        Model model = new ModelManager();
+        model.addCookbookRecipe(VALID_RECIPE);
+        model.setInventory(new Inventory());
+
         RecipeContainsInventoryIngredientsPredicate c =
             new RecipeContainsInventoryIngredientsPredicate(model.getInventory());
         assertFalse(c.test(VALID_RECIPE));
