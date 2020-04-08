13f4ef7753d98c799f4612c5fda9d20920a36280
gohkh <gohkh2011@gmail.com>
2020-04-08 12:07:46 +0800

    Remove redundant method

--- src/main/java/seedu/address/model/ingredient/UniqueIngredientList.java
+++ src/main/java/seedu/address/model/ingredient/UniqueIngredientList.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -170,15 +169,5 @@ public class UniqueIngredientList implements Iterable<Ingredient> {
     @Override
     public int hashCode() {
         return internalList.hashCode();
-    }
-
-    public List<String> getIngredientNamesString() {
-        StringBuilder sb = new StringBuilder();
-
-        for (Ingredient i : internalList) {
-            sb.append(i.getName().ingredientName).append(" ");
-        }
-
-        return Arrays.asList(sb.toString().split("\\s+"));
     }
 }
