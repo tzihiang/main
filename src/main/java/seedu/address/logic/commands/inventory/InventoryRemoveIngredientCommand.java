48bcad60b2186efbc92aa948a06acaa6eca0ba5c
gohkh <gohkh2011@gmail.com>
2020-04-09 16:34:47 +0800

    Modify error message

--- src/main/java/seedu/address/logic/commands/inventory/InventoryRemoveIngredientCommand.java
+++ src/main/java/seedu/address/logic/commands/inventory/InventoryRemoveIngredientCommand.java
@@ -23,7 +23,7 @@ public class InventoryRemoveIngredientCommand extends InventoryCommand {
 
     public static final String COMMAND_WORD = "remove ingredient";
     public static final String MESSAGE_SUCCESS = "%1$s removed from inventory";
-    public static final String MESSAGE_INGREDIENT_QUANTITY_TOO_HIGH = "The inventory does not contain %2$s %1$s";
+    public static final String MESSAGE_INGREDIENT_QUANTITY_TOO_HIGH = "The quantity specified is too large";
     public static final String MESSAGE_USAGE = COMMAND_CATEGORY + " " + COMMAND_WORD
             + ": This commands allows you to remove ingredients to your inventory.\n"
             + "Parameters for removing an ingredient into your inventory is as follows: \n"
@@ -62,8 +62,7 @@ public class InventoryRemoveIngredientCommand extends InventoryCommand {
 
             return new CommandResult(String.format(MESSAGE_SUCCESS, ingredientRemoved));
         } catch (NonPositiveIngredientQuantityException e) {
-            throw new CommandException(String.format(MESSAGE_INGREDIENT_QUANTITY_TOO_HIGH,
-                    ingredientName, ingredientQuantity.get()));
+            throw new CommandException(String.format(MESSAGE_INGREDIENT_QUANTITY_TOO_HIGH));
         }
     }
 
